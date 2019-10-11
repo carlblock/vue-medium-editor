e6e402607309ff2d7232f6781cafd3dba3d2af1b
Carl Blockås <carlblockas@gmail.com>
2019-10-11 12:33:18 +0200

    Support for webpack 4 and nodejs 10

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,55 @@
 const webpack = require('webpack')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path')
 const PRODUCTION = process.env.NODE_ENV === 'production'
 
-module.exports = function (options) {
-  options = options || {}
-  return {
+// module.exports = function (options) {
+//   options = options || {}
+//   return {
+//     entry: './index',
+//     resolve: {
+//       extensions: [ '.es6', '.js' ]
+//     },
+//     output: {
+//       filename: `vueMediumEditor${ PRODUCTION ? '.min' : '' }.js`,
+//       path: path.resolve(__dirname, 'dist'),
+//       library: 'vueMediumEditor',
+//       libraryTarget: 'umd'
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: PRODUCTION ? ['es2015', 'babili'] : ['es2015']
+//             }
+//           }
+//         }
+//       ]
+//     },
+//     plugins: PRODUCTION ? [ new UglifyJSPlugin() ] : []
+//   }
+// }
+module.exports = {
     entry: './index',
-    resolve: {
-      extensions: [ '.es6', '.js' ]
-    },
     output: {
-      filename: `vueMediumEditor${ PRODUCTION ? '.min' : '' }.js`,
-      path: path.resolve(__dirname, 'dist'),
-      library: 'vueMediumEditor',
-      libraryTarget: 'umd'
+        filename: `vueMediumEditor${ PRODUCTION ? '.min' : '' }.js`,
+        path: path.resolve(__dirname, 'dist'),
+        library: 'vueMediumEditor',
+        libraryTarget: 'umd'
     },
     module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: PRODUCTION ? ['es2015', 'babili'] : ['es2015']
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
-          }
-        }
-      ]
-    },
-    plugins: PRODUCTION ? [ new UglifyJSPlugin() ] : []
-  }
-}
+        ]
+    }
+};
